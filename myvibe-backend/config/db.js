cb6a7d39ca1416451a744bfe41c8ac40e3c3ff79
mudasarasim <saood822946@gmail.com>
2024-09-18 14:59:28 +0500

    db

--- myvibe-backend/config/db.js
+++ myvibe-backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/myvibe');
+    await mongoose.connect('mongodb+srv://khadijajutt173:dENk8V8a5Bt7bsyb@cluster0.3ycj6.mongodb.net/myvibe?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
